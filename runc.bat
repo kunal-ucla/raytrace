b9a03cc43158b9d9a89c454ddc6773b50d213ff7
timepiss <kunalkishore.mail@gmail.com>
2015-07-05 13:36:13 +0530

    "$@"

--- runc.bat
+++ runc.bat
@@ -2,7 +2,7 @@
 call cd C:\Users\Kunal\Desktop\CEWiT _Presentations\code
 call vcvars32
 if %1.==. (
-	set /p input=Which file do you want to run? 1 for test1.cpp and 2 for test2.cpp: 
+	set /p input=Which file do you want to run and compile? 1 for test1.cpp and 2 for test2.cpp: 
 ) else (
 	set input=%1
 )
