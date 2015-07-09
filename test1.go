12321471773406af22b417596fc6682596c71876
timepiss <kunalkishore.mail@gmail.com>
2015-07-09 14:27:32 +0530

    changed all parameters to UPPER CASE

--- test1.go
+++ test1.go
@@ -12,49 +12,60 @@ var plotcode int
 var PI float64 = 3.14159265
 var fid *os.File
 
+type Point3D []float64
+
+type Plane3D []Point3D
+
 type Object struct {
-	length, breadth, height   float64
-	position                  []float64
-	r_coeff, t_coeff, r_index float64
+	Length, Breadth, Height   float64
+	Position                  Point3D
+	R_coeff, T_coeff, R_index float64
 }
 
 type Receiver struct {
-	point  []float64
-	radius float64
+	Point  Point3D
+	Radius float64
 }
 
 type Transmitter struct {
-	point []float64
+	Point Point3D
 }
 
 type Ray struct {
-	point     []float64
-	direction []float64
+	Point     Point3D
+	Direction []float64
+	Id        int
 }
 
-func getEquations(i int, obj Object) []float64 {
+func DistanceBetweenPoints(p1, p2 Point3D) float64 {
+	var ans float64
+	ans = math.Sqrt(math.Pow(p1[0]-p2[0], 2) + math.Pow(p1[1]-p2[1], 2) + math.Pow(p1[2]-p2[2], 2))
+	return ans
+}
+
+func (obj Object) GetEquations(i int) []float64 {
 
 	var ans []float64
 	switch i {
 	case 0:
-		ans = []float64{0, 0, 1, -obj.position[2] + 0.5*obj.height}
+		ans = []float64{0, 0, 1, -obj.Position[2] + 0.5*obj.Height}
 	case 1:
-		ans = []float64{0, 0, 1, -obj.position[2] - 0.5*obj.height}
+		ans = []float64{0, 0, 1, -obj.Position[2] - 0.5*obj.Height}
 	case 2:
-		ans = []float64{0, 1, 0, -obj.position[1] + 0.5*obj.breadth}
+		ans = []float64{0, 1, 0, -obj.Position[1] + 0.5*obj.Breadth}
 	case 3:
-		ans = []float64{0, 1, 0, -obj.position[1] - 0.5*obj.breadth}
+		ans = []float64{0, 1, 0, -obj.Position[1] - 0.5*obj.Breadth}
 	case 4:
-		ans = []float64{1, 0, 0, -obj.position[0] + 0.5*obj.length}
+		ans = []float64{1, 0, 0, -obj.Position[0] + 0.5*obj.Length}
 	case 5:
-		ans = []float64{1, 0, 0, -obj.position[0] - 0.5*obj.length}
+		ans = []float64{1, 0, 0, -obj.Position[0] - 0.5*obj.Length}
 	}
 	return ans
 }
 
 //multiplies a vector(an array) by a scalar
 //the array should be of type []float64 only and the scalar should be of type float64 only
-func dot(a []float64, b float64) []float64 {
+func Dot(a []float64, b float64) []float64 {
 	var c = []float64{0, 0, 0}
 	for i := 0; i < 3; i++ {
 		c[i] = a[i] * b
@@ -64,7 +75,7 @@ func dot(a []float64, b float64) []float64 {
 
 //returns the sum of the elements of an array
 //the array should be of type []float64 only and the scalar should be of type float64 only
-func sum(a []float64) float64 {
+func Sum(a []float64) float64 {
 	var c float64
 	for i := 0; i < 3; i++ {
 		c = c + a[i]
@@ -74,7 +85,7 @@ func sum(a []float64) float64 {
 
 //returns the dot product of two vectors(arrays)
 //the array should be of type []float64 only
-func dot2(a, b []float64) []float64 {
+func Dot2(a, b []float64) []float64 {
 	var c = []float64{0, 0, 0}
 	for i := 0; i < 3; i++ {
 		c[i] = a[i] * b[i]
@@ -84,7 +95,7 @@ func dot2(a, b []float64) []float64 {
 
 //returns the sum of two vectors(arrays)
 //the array should be of type []float64 only
-func sum2(a, b []float64) []float64 {
+func Sum2(a, b []float64) []float64 {
 	var c = []float64{0, 0, 0}
 	for i := 0; i < 3; i++ {
 		c[i] = a[i] + b[i]
@@ -92,13 +103,13 @@ func sum2(a, b []float64) []float64 {
 	return c
 }
 
-func isItInside(point []float64, obj Object) int {
+func (obj Object) IsItInside(point Point3D) int {
 	check := 0
 	for i := 0; i < 6; i++ {
-		woo1 := dot2(getEquations(i, obj), obj.position)
-		woo2 := dot2(getEquations(i, obj), point)
-		acc1 := getEquations(i, obj)[3] + sum(woo1)
-		acc2 := getEquations(i, obj)[3] + sum(woo2)
+		woo1 := Dot2(obj.GetEquations(i), obj.Position)
+		woo2 := Dot2(obj.GetEquations(i), point)
+		acc1 := obj.GetEquations(i)[3] + Sum(woo1)
+		acc2 := obj.GetEquations(i)[3] + Sum(woo2)
 		if acc1*acc2 >= 0 {
 			//do nothing
 		} else {
@@ -112,15 +123,15 @@ func isItInside(point []float64, obj Object) int {
 	}
 }
 
-func getPOI(ray Ray, obj Object) []float64 {
+func GetPOI(ray Ray, obj Object) []float64 {
 	t := 0.0
 	i_min := -1
 	count := 0
 	for ii := 0; ii < 6; ii++ {
-		acc1 := sum(dot2(getEquations(ii, obj), ray.point))
-		acc2 := sum(dot2(getEquations(ii, obj), ray.direction))
-		tt := -1.0 * (getEquations(ii, obj)[3] + acc1) / acc2
-		if tt > 0 && isItInside(sum2(ray.point, dot(ray.direction, tt)), obj) == 1 {
+		acc1 := Sum(Dot2(obj.GetEquations(ii), ray.Point))
+		acc2 := Sum(Dot2(obj.GetEquations(ii), ray.Direction))
+		tt := -1.0 * (obj.GetEquations(ii)[3] + acc1) / acc2
+		if tt > 0 && obj.IsItInside(Sum2(ray.Point, Dot(ray.Direction, tt))) == 1 {
 			if count == 0 {
 				i_min = ii
 				t = tt
@@ -135,14 +146,14 @@ func getPOI(ray Ray, obj Object) []float64 {
 	ip[3] = t
 	ip[4] = float64(i_min)
 	var safe_ip = []float64{0, 0, 0}
-	safe_ip = sum2(ray.point, dot(ray.direction, t))
+	safe_ip = Sum2(ray.Point, Dot(ray.Direction, t))
 	for i := 0; i < 3; i++ {
 		ip[i] = safe_ip[i]
 	}
 	return ip
 }
 
-func isSameSide(p1 []float64, p2 []float64, plane []float64) float64 {
+func IsSameSide(p1 []float64, p2 []float64, plane []float64) float64 {
 	check := (plane[0]*p1[0] + plane[1]*p1[1] + plane[2]*p1[2] + plane[3]) * (plane[0]*p2[0] + plane[1]*p2[1] + plane[2]*p2[2] + plane[3])
 	if check > 0 {
 		return 1.0
@@ -151,11 +162,11 @@ func isSameSide(p1 []float64, p2 []float64, plane []float64) float64 {
 	}
 }
 
-func doesItPass(ray Ray, rec Receiver) []float64 {
+func DoesItPass(ray Ray, rec Receiver) []float64 {
 	var ans = []float64{0, 0}
-	ans[0] = sum(dot2(sum2(rec.point, dot(ray.point, -1.0)), ray.direction)) / sum(dot2(ray.direction, ray.direction))
-	p := sum2(sum2(ray.point, dot(rec.point, -1.0)), dot(ray.direction, ans[0]))
-	if math.Sqrt(sum(dot2(p, p))) <= rec.radius && ans[0] > 0.0 {
+	ans[0] = Sum(Dot2(Sum2(rec.Point, Dot(ray.Point, -1.0)), ray.Direction)) / Sum(Dot2(ray.Direction, ray.Direction))
+	p := Sum2(Sum2(ray.Point, Dot(rec.Point, -1.0)), Dot(ray.Direction, ans[0]))
+	if math.Sqrt(Sum(Dot2(p, p))) <= rec.Radius && ans[0] > 0.0 {
 		ans[1] = 1
 	} else {
 		ans[1] = 0
@@ -163,13 +174,13 @@ func doesItPass(ray Ray, rec Receiver) []float64 {
 	return ans
 }
 
-func nextObject(presentIndex int, ray Ray, obstacles []Object) []float64 {
+func NextObject(presentIndex int, ray Ray, obstacles []Object) []float64 {
 	count := 0
 	index := -1
 	iii := -1
 	t := 0.0
 	for i := 0; i < len(obstacles); i++ {
-		poi := getPOI(ray, obstacles[i])
+		poi := GetPOI(ray, obstacles[i])
 		var poi_4 = []float64{0, 0, 0, 0}
 		for poi_i := 0; poi_i < 3; poi_i++ {
 			poi_4[poi_i] = poi[poi_i]
@@ -198,11 +209,11 @@ func nextObject(presentIndex int, ray Ray, obstacles []Object) []float64 {
 	return ans
 }
 
-func getPoints(obj Object) {
+func (obj Object) GetPoints() [][]Point3D {
 
 	sign := [][]float64{{1.0, -1.0, -1.0, 1.0}, {1.0, 1.0, -1.0, -1.0}}
-	size := []float64{obj.length / 2.0, obj.breadth / 2.0, obj.height / 2.0}
-	pos := []float64{obj.position[0], obj.position[1], obj.position[2]}
+	size := []float64{obj.Length / 2.0, obj.Breadth / 2.0, obj.Height / 2.0}
+	pos := []float64{obj.Position[0], obj.Position[1], obj.Position[2]}
 	for i := 0; i < 3; i++ {
 		for j := -1; j <= 1; j = j + 2 {
 			t := make([][][]float64, len(data.Planes)+1)
@@ -224,12 +235,12 @@ func getPoints(obj Object) {
 	}
 }
 
-func raytrace(ray Ray, fieldStrength float64, pathLength float64, obstacles []Object, presentIndex int) int {
+func Raytrace(ray Ray, fieldStrength float64, pathLength float64, obstacles []Object, presentIndex int) int {
 	var index, iii int
 	t := 0.0
 
 	//find the next obstacle in the path of the ray:
-	indices := nextObject(-1, ray, obstacles)
+	indices := NextObject(-1, ray, obstacles)
 	t = indices[0]
 	iii = int(indices[1])
 	index = int(indices[2])
@@ -239,7 +250,7 @@ func raytrace(ray Ray, fieldStrength float64, pathLength float64, obstacles []Ob
 	}
 
 	//check if it enters receiver region:
-	receiverCheck := doesItPass(ray, receiver)
+	receiverCheck := DoesItPass(ray, receiver)
 	didItReach := 0
 	if receiverCheck[1] == 1 && receiverCheck[0] < t {
 		t = receiverCheck[0]
@@ -247,16 +258,16 @@ func raytrace(ray Ray, fieldStrength float64, pathLength float64, obstacles []Ob
 	}
 
 	//find the point of intersection and set it for next rays:
-	p := sum2(ray.point, dot(ray.direction, t))
-	pathLength = pathLength + t*math.Sqrt(sum(dot2(ray.direction, ray.direction)))
+	p := Sum2(ray.Point, Dot(ray.Direction, t))
+	pathLength = pathLength + t*math.Sqrt(Sum(Dot2(ray.Direction, ray.Direction)))
 	//var bufferRay Ray
-	reflectedRay := Ray{point: p}
-	transmittedRay := Ray{point: p}
+	reflectedRay := Ray{Point: p}
+	transmittedRay := Ray{Point: p}
 	small_t := 0.01
-	pp := sum2(p, dot(ray.direction, small_t))
+	pp := Sum2(p, Dot(ray.Direction, small_t))
 	nextIndex := 0
 	for lo := 0; lo < len(obstacles); lo++ {
-		if isItInside(pp, obstacles[lo]) == 1 && lo != 0 {
+		if obstacles[lo].IsItInside(pp) == 1 && lo != 0 {
 			nextIndex = lo
 		}
 	}
@@ -274,7 +285,7 @@ func raytrace(ray Ray, fieldStrength float64, pathLength float64, obstacles []Ob
 			t := make([][]float64, len(data.Points)+2)
 			copy(t, data.Points)
 			data.Points = t
-			data.Points[len(t)-2] = []float64{ray.point[0], ray.point[1], ray.point[2], float64(plotcode)}
+			data.Points[len(t)-2] = []float64{ray.Point[0], ray.Point[1], ray.Point[2], float64(plotcode)}
 			data.Points[len(t)-1] = []float64{p[0], p[1], p[2], float64(plotcode)}
 		}
 		fmt.Print("|")
@@ -292,46 +303,48 @@ func raytrace(ray Ray, fieldStrength float64, pathLength float64, obstacles []Ob
 	//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!!!!TO DO!!!!---find exact point where the ray dies----------------------------------------
 
 	//set the direction ratios of the reflected ray here:
-	acc1 := sum(dot2(ray.direction, ray.direction))
-	acc2 := -1.0 * (sum(dot2(ray.direction, getEquations(iii, obstacles[index]))))
-	acc3 := sum(dot2(getEquations(iii, obstacles[index]), getEquations(iii, obstacles[index]))) - math.Pow(getEquations(iii, obstacles[index])[3], 2)
+	acc1 := Sum(Dot2(ray.Direction, ray.Direction))
+	acc2 := -1.0 * (Sum(Dot2(ray.Direction, obstacles[index].GetEquations(iii))))
+	acc3 := Sum(Dot2(obstacles[index].GetEquations(iii), obstacles[index].GetEquations(iii))) - math.Pow(obstacles[index].GetEquations(iii)[3], 2)
 	t = acc2 / acc1
-	normal_check1 := sum2(p, dot(ray.direction, small_t))
-	normal_check2 := sum2(p, dot(getEquations(iii, obstacles[index]), small_t))
-	f_normal := -1.0 * isSameSide(normal_check1, normal_check2, getEquations(iii, obstacles[index]))
+	normal_check1 := Sum2(p, Dot(ray.Direction, small_t))
+	normal_check2 := Sum2(p, Dot(obstacles[index].GetEquations(iii), small_t))
+	f_normal := -1.0 * IsSameSide(normal_check1, normal_check2, obstacles[index].GetEquations(iii))
 	i_dot_n := 2.0 * acc2 // * f_normal;
-	ans := sum2(dot(getEquations(iii, obstacles[index]), i_dot_n), ray.direction)
-	reflectedRay.direction = ans
+	ans := Sum2(Dot(obstacles[index].GetEquations(iii), i_dot_n), ray.Direction)
+	reflectedRay.Direction = ans
 
 	//set the direction ratios of the transmitted ray here:
 	cosTheta1 := -acc2 / math.Sqrt(acc1*acc3)
 	sinTheta1 := math.Sin(math.Acos(cosTheta1))
-	n := obstacles[presentIndex].r_index / obstacles[nextIndex].r_index
+	n := obstacles[presentIndex].R_index / obstacles[nextIndex].R_index
 	sinTheta2 := n * sinTheta1
 	//cosTheta2 := math.Cos(math.Asin(sinTheta2))
 	t_c := -1.0 * math.Sqrt(1-math.Pow(sinTheta2, 2)) / math.Sqrt(acc3)
 	lollipop := f_normal * cosTheta1 / math.Sqrt(acc3)
-	t_par := dot(sum2(dot(ray.direction, 1/math.Sqrt(acc1)), dot(getEquations(iii, obstacles[index]), lollipop)), 2)
-	t_per := dot(getEquations(iii, obstacles[index]), f_normal*t_c)
-	t_total := sum2(t_par, t_per)
-	transmittedRay.direction = t_total
+	t_par := Dot(Sum2(Dot(ray.Direction, 1/math.Sqrt(acc1)), Dot(obstacles[index].GetEquations(iii), lollipop)), 2)
+	t_per := Dot(obstacles[index].GetEquations(iii), f_normal*t_c)
+	t_total := Sum2(t_par, t_per)
+	transmittedRay.Direction = t_total
 
 	//trace the reflected and refracted rays next:
 	ref_return := 1
 	if index == 0 || presentIndex == 0 {
-		ref_return = raytrace(reflectedRay, obstacles[presentIndex].r_coeff*fieldStrength, pathLength, obstacles, presentIndex)
+		ref_return = Raytrace(reflectedRay, obstacles[presentIndex].R_coeff*fieldStrength, pathLength, obstacles, presentIndex)
 	}
-	trans_return := raytrace(transmittedRay, obstacles[index].t_coeff*fieldStrength, pathLength, obstacles, nextIndex)
+	trans_return := Raytrace(transmittedRay, obstacles[index].T_coeff*fieldStrength, pathLength, obstacles, nextIndex)
 	if ref_return*trans_return >= 2 {
 		{
 			t := make([][]float64, len(data.Points)+2)
 			copy(t, data.Points)
 			data.Points = t
-			data.Points[len(t)-2] = []float64{ray.point[0], ray.point[1], ray.point[2], float64(plotcode)}
+			data.Points[len(t)-2] = []float64{ray.Point[0], ray.Point[1], ray.Point[2], float64(plotcode)}
 			data.Points[len(t)-1] = []float64{p[0], p[1], p[2], float64(plotcode)}
 		}
 		plotcode++
 	}
+
+	// ch <- ray
 	return ref_return * trans_return
 }
 
@@ -345,9 +358,9 @@ type Data struct {
 
 var data Data
 
-var receiver Receiver = Receiver{point: []float64{-5, -3.08, 0}, radius: 0.2241}
+var receiver Receiver = Receiver{Point: []float64{-5, -3.08, 0}, Radius: 0.2241}
 
-var transmitter Transmitter = Transmitter{point: []float64{-2.0174, -2.58, 0}}
+var transmitter Transmitter = Transmitter{Point: []float64{-2.0174, -2.58, 0}}
 
 func main() {
 
@@ -355,26 +368,26 @@ func main() {
 
 	fid, _ = os.Create("out.json")
 
-	data.Receiver = []float64{receiver.point[0], receiver.point[1], receiver.point[2], receiver.radius}
-	data.Transmitter = transmitter.point
+	data.Receiver = []float64{receiver.Point[0], receiver.Point[1], receiver.Point[2], receiver.Radius}
+	data.Transmitter = transmitter.Point
 
-	Room := Object{length: 13, breadth: 8.6, height: 3, r_coeff: 0.4, t_coeff: 0, r_index: 1, position: []float64{0, 0, 0}}
-	getPoints(Room)
+	Room := Object{Length: 13, Breadth: 8.6, Height: 3, R_coeff: 0.4, T_coeff: 0, R_index: 1, Position: []float64{0, 0, 0}}
+	Room.GetPoints()
 
-	Box := Object{length: 0.03, breadth: 2.5, height: 3, r_coeff: 0.4, t_coeff: 0, r_index: 2, position: []float64{-3.6586, -2.163, 0}}
-	getPoints(Box)
+	Box := Object{Length: 0.03, Breadth: 2.5, Height: 3, R_coeff: 0.4, T_coeff: 0, R_index: 2, Position: []float64{-3.6586, -2.163, 0}}
+	Box.GetPoints()
 
-	Box1 := Object{length: 0.91, breadth: 0.645, height: 3, r_coeff: 0.4, t_coeff: 0, r_index: 2, position: []float64{6.045, 3.9775, 0}}
-	getPoints(Box1)
+	Box1 := Object{Length: 0.91, Breadth: 0.645, Height: 3, R_coeff: 0.4, T_coeff: 0, R_index: 2, Position: []float64{6.045, 3.9775, 0}}
+	Box1.GetPoints()
 
-	Box2 := Object{length: 0.91, breadth: 0.645, height: 3, r_coeff: 0.4, t_coeff: 0, r_index: 2, position: []float64{-6.045, 3.9775, 0}}
-	getPoints(Box2)
+	Box2 := Object{Length: 0.91, Breadth: 0.645, Height: 3, R_coeff: 0.4, T_coeff: 0, R_index: 2, Position: []float64{-6.045, 3.9775, 0}}
+	Box2.GetPoints()
 
-	Box3 := Object{length: 0.91, breadth: 0.645, height: 3, r_coeff: 0.4, t_coeff: 0, r_index: 2, position: []float64{6.045, -3.9775, 0}}
-	getPoints(Box3)
+	Box3 := Object{Length: 0.91, Breadth: 0.645, Height: 3, R_coeff: 0.4, T_coeff: 0, R_index: 2, Position: []float64{6.045, -3.9775, 0}}
+	Box3.GetPoints()
 
-	Box4 := Object{length: 0.91, breadth: 0.645, height: 3, r_coeff: 0.4, t_coeff: 0, r_index: 2, position: []float64{-6.045, -3.9775, 0}}
-	getPoints(Box4)
+	Box4 := Object{Length: 0.91, Breadth: 0.645, Height: 3, R_coeff: 0.4, T_coeff: 0, R_index: 2, Position: []float64{-6.045, -3.9775, 0}}
+	Box4.GetPoints()
 
 	var obstacles []Object = []Object{Room, Box, Box1, Box2, Box3, Box4}
 
@@ -383,20 +396,24 @@ func main() {
 
 	//somehow start many rays from transmitter
 	fR := 0.6
-	fA := 0.001
-	fB := 0.001
+	fA := 0.05
+	fB := 0.05
 	count_rays := 0
 	for fi := -fB * float64(int(fR/fB)); fi <= fR; fi = fi + fB {
 		fr := math.Sqrt(math.Pow(fR, 2) - math.Pow(fi, 2))
 		for fa := 0.0; fa <= 2*PI; fa = fa + fA/fr {
-			rayX := Ray{point: transmitter.point, direction: []float64{fr * math.Cos(fa), fr * math.Sin(fa), fi}}
-			raytrace(rayX, 1, 0, obstacles, 0)
+			rayX := Ray{Point: transmitter.Point, Direction: []float64{fr * math.Cos(fa), fr * math.Sin(fa), fi}}
+			go func(Ray, chan Ray) {
+				Raytrace(rayX, 1, 0, obstacles, 0)
+				ch <- rayX
+			}(rayX, ch)
 			count_rays++
 		}
 	}
 
-	pinbytes, _ := json.Marshal(data)
-
+	pinbytes, _ := json.MarshalIndent(data, "", "\t")
+	// jd:= json.NewEncoder(fid)
+	// json.MarshalIndent(v, prefix, indent)
 	fmt.Fprintln(fid, string(pinbytes))
 	fid.Close()
 	elapsed := time.Since(start)
